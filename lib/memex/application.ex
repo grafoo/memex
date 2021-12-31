a2314ac958e55dc0e1b1e17eaab7e35db4617240
Manuel Graf <postmaster@grafoo.at>
2021-12-31 14:44:31 +0100

    add mix project

---
+++ lib/memex/application.ex
@@ -0,0 +1,20 @@
+defmodule Memex.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Memex.Worker.start_link(arg)
+      # {Memex.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Memex.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
